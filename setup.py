ab54641c5dd1b74df376a117c47550dc14d0b9ad
leepand <leepand@leepanddeMacBook-Pro.local>
2023-10-09 14:40:51 +0800

    first commit

--- setup.py
+++ setup.py
@@ -1,52 +1,41 @@
 import os
-from setuptools import setup
+from setuptools import find_packages, setup
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 # read the docs could not compile numpy and c extensions
 if on_rtd:
     setup_requires = []
     install_requires = []
 else:
-    setup_requires = [
-        'nose',
-        'coverage',
-    ]
     install_requires = [
-        'six',
-        'numpy',
-        'scipy',
-        'matplotlib',
+        "six",
+        "numpy",
+        "scipy",
+        "matplotlib",
     ]
 
-long_description = ("See `github <https://github.com/leepand/RLXtreme>`_ "
-                    "for more information.")
+long_description = (
+    "See `github <https://github.com/leepand/RLXtreme>`_ " "for more information."
+)
 
 setup(
-    name='RLXtreme',
-    version='0.2.5',
-    description='Contextual bandit/RL in python',
+    name="RLXtreme",
+    version="0.0.1",
+    description="Contextual bandit/RL in python",
     long_description=long_description,
-    author='Leepand',
-    author_email='pandeng.li@163.com',
-    url='https://github.com/leepand/RLXtreme',
-    setup_requires=setup_requires,
+    author="Leepand",
+    license="Apache 2.0",
+    author_email="pandeng.li@163.com",
+    url="https://github.com/leepand/RLXtreme",
     install_requires=install_requires,
+
     classifiers=[
-        'Topic :: Scientific/Engineering',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3",
     ],
-    test_suite='nose.collector',
-    packages=[
-        'rlxtreme',
-        'rlxtreme.agent',
-        'rlxtreme.storage',
-        'rlxtreme.utils',
-    ],
-    package_dir={
-        'rlxtreme': 'rlxtreme',
-        'rlxtreme.agent': 'rlxtreme/agent',
-        'rlxtreme.storage': 'rlxtreme/storage',
-        'rlxtreme.utils': 'rlxtreme/utils',
-    },
+    test_suite="nose.collector",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
 )

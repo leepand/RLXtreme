2f7055a1ee3a10ff59c2c7ec04dc1bc536951448
leepand <leepand@leepanddeMacBook-Pro.local>
2023-10-09 16:23:19 +0800

    add base agent

--- rlxtreme/storage/__init__.py
+++ rlxtreme/storage/__init__.py
@@ -4,4 +4,4 @@
 from .recom import Recom
 from .history import History, MemoryHistoryStorage, RliteHistoryStorage
 from .model import MemoryModelStorage, RliteModelStorage
-from .action import Action, MemoryActionStorage
+from .action import Action, MemoryActionStorage, RliteActionStorage
